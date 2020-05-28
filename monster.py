67e48dadc5ffa88176f72ae9bd1bee0d29cb20c9
pjkpjk900 <pk58@buffalo.edu>
2020-05-28 09:51:06 -0400

    Imported BeautifulSoup

--- monster.py
+++ monster.py
@@ -1,8 +1,9 @@
 import requests
-
+from bs4 import BeautifulSoup
 
 url = 'https://www.monster.com/jobs/search/?q=Software-Developer&where=New-York-City__2C-NY'
 page = requests.get(url)
 
-print(page.content)
+soup = BeautifulSoup(page.content, 'html.parser')
+print(soup)
 
